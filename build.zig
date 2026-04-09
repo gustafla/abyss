765908ae47b37787102f584860d6aa09f262b97d
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2026-04-09 17:29:54 +0300

    Fix rpath

--- build.zig
+++ build.zig
@@ -2,15 +2,16 @@ const std = @import("std");
 const engine = @import("mehustin2");
 
 pub fn build(b: *std.Build) void {
+    // Initialize options and dependency
     const options = engine.initOptions(b);
     const engine_dep = b.dependency("mehustin2", options);
 
     const engine_mod = engine_dep.module("engine");
-
     const script_mod = b.createModule(.{
         .root_source_file = b.path("src/script.zig"),
     });
 
+    // Hook up module dependencies
     script_mod.addImport("engine", engine_mod);
     engine_mod.addImport("script", script_mod);
     engine_dep.module("render").addImport("script", script_mod);
@@ -19,14 +20,6 @@ pub fn build(b: *std.Build) void {
     // Compile and install shaders
     engine.compileShaders(b, engine_dep, @import("src/config.zon"));
 
-    // Extract and install the artifacts the engine builds
-    const demo_exe = engine_dep.artifact(options.exe_name);
-    b.installArtifact(demo_exe);
-    const render_lib = engine_dep.artifact("render");
-    b.installArtifact(render_lib);
-
-    const run_cmd = b.addRunArtifact(demo_exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_cmd.step);
+    // Install the build artifacts
+    engine.install(b, engine_dep, options);
 }
